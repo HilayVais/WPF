a5c39414a2d2877d26ca5acf3b24319d9d545a27
hilayv <vaish@post.bgu.ac.il>
2018-05-14 12:11:07 +0300

    create

---
+++ DispatcherAndBinding/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using DispatcherAndBinding;
+
+namespace DispatcherAndBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static Random random = new Random();
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        ObservableObject _main = new ObservableObject();
+        public MainWindow()
+        {
+            InitializeComponent();            
+            this.DataContext = _main;
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_main.Login(username.Text, groupID.Text))
+                MessageBox.Show(this, "wrong nickname or group  ID, try again");
+            else
+            {
+                MessageBox.Show(this, "Logging in! you will be moved to chatroon window now");
+                //this.Close();
+                Chat w = new Chat(this._main);
+                w.Show();
+            }
+
+            }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+                }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            //do register to obs
+        }
+
+
+        
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DispatcherAndBinding/ObservableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DispatcherAndBinding
+{
+    public class ObservableObject 
+    {
+        public ObservableObject()
+        {
+            //create the chat room
+        }
+       
+        public bool Login(string nickname, string groupID) {//try to log in
+   
+            if (nickname == "hilay" & groupID == "vais")
+                return true;
+            return false;
+        }
+        public void sendMessage(string msg)
+        {
+            //send the message
+        }
+
+    }
+}
